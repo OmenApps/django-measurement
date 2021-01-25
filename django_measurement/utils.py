35cf67376ea70aef260ffc94988b23d6db845bb3
Omen Apps <support@omenapps.com>
2021-01-24 21:15:13 -0500

    Updated to use the measure's base unit

--- django_measurement/utils.py
+++ django_measurement/utils.py
@@ -7,5 +7,5 @@ from measurement.base import AbstractMeasure
 def get_measurement(
     measure: Type[AbstractMeasure], value: decimal.Decimal
 ) -> AbstractMeasure:
-    unit = next(iter(measure._units.keys()))
+    unit = next(iter(measure.get_base_unit_names(measure)))
     return measure(f"{value} {unit}")
